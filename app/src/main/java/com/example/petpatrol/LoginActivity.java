b696fc72ba9691bef56a9b8c6c3bb4edc95143e1
kkreitmair <s-kkreit@haw-landshut.de>
2019-11-27 17:26:04 +0100

    login: refactore layout resources
    Signed-off-by: kkreitmair <s-kkreit@haw-landshut.de>

--- app/src/main/java/com/example/petpatrol/LoginActivity.java
+++ app/src/main/java/com/example/petpatrol/LoginActivity.java
@@ -38,7 +38,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.login);
 
         initializeUserInfo();
         activateOnClickListeners();
