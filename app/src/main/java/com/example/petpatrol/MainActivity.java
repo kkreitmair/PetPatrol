53d02a49b56331cf5bb3f950910cf5e2f645eb90
kkreitmair <s-kkreit@haw-landshut.de>
2019-11-27 17:27:43 +0100

    toolbar: add costum toolbar to main activity
    Signed-off-by: kkreitmair <s-kkreit@haw-landshut.de>

--- app/src/main/java/com/example/petpatrol/MainActivity.java
+++ app/src/main/java/com/example/petpatrol/MainActivity.java
@@ -1,14 +1,14 @@
 package com.example.petpatrol;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.widget.LinearLayout;
-
-import com.example.petpatrol.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,12 +17,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_view);
+        Toolbar myToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+
         exampleView();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
     private void exampleView() {
-        setContentView(R.layout.main_view);
         inAppScrollView = findViewById(R.id.inAppScrollView);
         for (int index=0; index <=4; index++) {
             generateAdvert();
